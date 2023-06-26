89ceb011cb0020bafea65dbb2de7507773717b70
Shirtsy <48637891+Shirtsy@users.noreply.github.com>
2023-06-26 04:01:17 -0700

    Updated to use the latest GPT-4 models

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -15,8 +15,8 @@ from calendar import monthrange
 # Models can be found here: https://platform.openai.com/docs/models/overview
 GPT_3_MODELS = ("gpt-3.5-turbo", "gpt-3.5-turbo-0301")
 GPT_3_16K_MODELS = ("gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613")
-GPT_4_MODELS = ("gpt-4", "gpt-4-0314")
-GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0314")
+GPT_4_MODELS = ("gpt-4", "gpt-4-0613")
+GPT_4_32K_MODELS = ("gpt-4-32k", "gpt-4-32k-0613")
 GPT_ALL_MODELS = GPT_3_MODELS + GPT_3_16K_MODELS + GPT_4_MODELS + GPT_4_32K_MODELS
 
 
